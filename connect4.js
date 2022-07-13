700761f55097bb7dfcfcc88171077cde5fd162a8
Billyhrussell <billy.hrussell@gmail.com>
2022-07-13 16:10:45 -0700

    added valid wins to checkForWin

--- connect4.js
+++ connect4.js
@@ -139,12 +139,8 @@ function checkForWin() {
   // using HEIGHT and WIDTH, generate "check list" of coordinates
   // for 4 cells (starting here) for each of the different
   // ways to win: horizontal, vertical, diagonalDR, diagonalDL
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      // TODO: assign values to the below variables for each of the ways to win
-      // horizontal has been assigned for you
-      // each should be an array of 4 cell coordinates:
-      // [ [y, x], [y, x], [y, x], [y, x] ]
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
 
       let horiz = [
         [y, x],
@@ -152,9 +148,24 @@ function checkForWin() {
         [y, x + 2],
         [y, x + 3],
       ];
-      let vert;
-      let diagDL;
-      let diagDR;
+      let vert = [
+        [y,x],
+        [y+1, x],
+        [y+2, x],
+        [y+3, x],
+      ];
+      let diagDL = [
+        [y,x],
+        [y+1, x+1],
+        [y+2, x+2],
+        [y+3, x+3],
+      ];
+      let diagDR = [
+        [y,x],
+        [y-1, x+1],
+        [y-2, x+2],
+        [y-3, x+3],
+      ];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
